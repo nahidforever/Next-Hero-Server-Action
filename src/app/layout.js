ad0459b4078b1439529c7c4878cdd532fa12207e
Nahid1828 <isam15-5334@diu.edu.bd>
2026-04-21 16:18:00 +0600

    Navigation bar and set theme on the website

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/componnents/Navbar";
+import NextThemeProviders from "@/providers/NextThemeProviders";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,9 +22,15 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      suppressHydrationWarning
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased bg-background text-foreground`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <NextThemeProviders>
+          <Navbar></Navbar>
+          <main>{children}</main>
+        </NextThemeProviders>
+      </body>
     </html>
   );
 }
